ed518f31c4420de461ae1b0406587cd057130b82
scrucher <jhonsteph666@gmail.com>
2021-05-24 03:49:05 +0100

    first push

---
+++ ReadMe.md
@@ -0,0 +1,91 @@
+This Is the Detailed Dashboard 
+
+#############################################
+
+the Code in src Directory developed to handle:
+
+- the request comming from website (order a pack, contact Form, and Save newsLetter Emails)
+- Register User, Authenticate 
+- Update User Data still in Progress
+- Integrate with API to Display google Analytics Data 
+- Generate QR Code 
+
+#############################################
+
+
+
+User Class: 
+
+here you can find All the methods related to user like 
+register method : Working 
+loging Method : Working
+Authenticate Mothods [isloggedin, isAdmin] : Working
+
+Update User Data ; Finished but not tested 
+Delete User : Finished But not Tested 
+
+/////////////////////////////////////////
+
+Packs / Offers Class: 
+
+here you can find all about requeting a pack: 
+
+the whole class is working ...
+
+feature delete
+
+////////////////////////////////
+
+Contact Class: 
+
+here you can find all about requeting a pack: 
+
+the whole class is working ...
+
+feature delete
+
+//////////////////////////////
+
+Newsletter Class: 
+
+here you can find all about requeting a pack: 
+
+the whole class is working ...
+
+feature delete
+
+#################################################
+
+Under the src Directory
+
+///////////////////////
+
+inter.php file 
+
+this file was meant to integrate with the web page: 
+
+the code has been written But not developed 
+
+/////////////////////
+
+comp.php file 
+
+this file was made to generate QR code but Something Went wrong with The Composer Library 
+it Would be fixed Soon 
+
+/////////////
+
+oauth2callback.php
+
+this file is Downloaded From google anlytics to Integrate With Google Api then the dashboard
+
+#######################################
+
+
+this Project is developed by Scrucher(Faical EL Jabli );
+
+Starting day : May 17th 2021
+
+DeadLine : May 31st 2021
+
+i didn't upload the fron't end part since it's not mine i mean didn't contribute in it and it's not for free usage 
---
+++ register.php
@@ -0,0 +1,108 @@
+<?php
+include_once'src/user.class.php';
+
+$usr = new User();
+
+if (isset($_POST['signup']))
+{
+    $usr->register();
+}
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="assets/csss/style.css">
+</head>
+<body>
+
+    <div class="main">
+
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">Sign up</h2>
+                        <?php
+                            $usr->display_error();
+                            
+                        ?>
+                        <form method="POST" class="register-form" id="register-form">
+                            <div class="form-group">
+                                <label for="username"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="username" id="username" placeholder="username"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="name" id="name" placeholder="First Name"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="lastname"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="lastname" id="lastname" placeholder="Last Name"/>
+                            </div>
+                         
+                            <div class="form-group">
+                                <label for="email"><i class="zmdi zmdi-email"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="Address"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="address" id="Address" placeholder="Address"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="pass" id="pass" placeholder="Password"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="text" name="role" id="re_pass" placeholder="Type Tou Current Position"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="text" name="city" id="re_pass" placeholder="Type Your City"/>
+                            </div>
+                       
+                            <div class="form-group form-button">
+                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="images/signup-image.png" alt="sing up image"></figure>
+                        <a href="#" class="signup-image-link">I am already member</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+       
+    </div>
+
+    <!-- JS -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/jss/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
